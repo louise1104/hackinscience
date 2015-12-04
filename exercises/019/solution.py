53b947cca334d7b2df067f1d9bc08d2ccc943c58
Louise Jacquot <louise.jacquot@cri-paris.org>
2015-12-04 16:25:22 +0100

    019

--- exercises/019/solution.py
+++ exercises/019/solution.py
@@ -2,4 +2,4 @@ import sys
 import operator
 if len(sys.argv) > 1:
     print(sys.argv[2] + sys.argv[3])
-else: print(usage: python3 solution.py OP1 OP2)
+else: print('usage: python3 solution.py OP1 OP2')
